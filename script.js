74bc97bf0f0cac8853aabe358650ca5ee2423827
samro <sam.rotthier@student.kdg.be>
2025-01-26 16:03:43 +0100

    Small changes to layout and data

--- script.js
+++ script.js
@@ -11,44 +11,43 @@ const data = [
 ]
 
 
-net.train(data)
+net.train(data);
 
 
-const colorEl = document.getElementById('color')
-const guessEl = document.getElementById('guess')
-const printOutput = document.getElementById('printOutput')
-const Prediction = document.getElementById('Prediction')
-const whiteButton = document.getElementById('white-button')
-const blackButton = document.getElementById('black-button')
-const printButton = document.getElementById('print-button')
+const colorEl = document.getElementById('color');
+const guessEl = document.getElementById('guess');
+const printOutput = document.getElementById('printOutput');
+const Prediction = document.getElementById('Prediction');
+const whiteButton = document.getElementById('white-button');
+const blackButton = document.getElementById('black-button');
+const printButton = document.getElementById('print-button');
 
-let color
-setRandomColor()
+let color;
+setRandomColor();
 
 whiteButton.addEventListener('click',() =>{
-    chooseColor(1)
-})
+    chooseColor(1);
+});
 
 blackButton.addEventListener('click',() =>{
-    chooseColor(0)
-})
+    chooseColor(0);
+});
 
 printButton.addEventListener('click',() =>{
-    print()
-    
-})
+    print();
+});
 
 function chooseColor(value){
     data.push({
         input:color,
         output:[value]
-    })
-    setRandomColor()
+    });
+    setRandomColor();
 }
 
 function print(){
-    console.log(JSON.stringify(data))
-    printOutput.innerHTML = "data = " + JSON.stringify(data)
+    console.log(JSON.stringify(data));
+    printOutput.innerHTML = "data = " + JSON.stringify(data);
 }
 
 function setRandomColor(){
@@ -57,8 +56,8 @@ function setRandomColor(){
         g:Math.random(),
         b:Math.random()
     }
-    const guess = net.run(color)[0]
-    Prediction.innerHTML = "Prediction: " + net.run(color)[0]
-    guessEl.style.color= guess > 0.5?'#FFF': '#000' //If guess is larger then 0.5 is white and otherwise black
-    colorEl.style.backgroundColor= `rgba(${color.r*255}, ${color.g*255}, ${color.b*255})`
+    const guess = net.run(color)[0];
+    Prediction.innerHTML = "Prediction: " + net.run(color)[0];
+    guessEl.style.color= guess > 0.5?'#FFF': '#000'; //If guess is larger then 0.5 is white and otherwise black
+    colorEl.style.backgroundColor= `rgba(${color.r*255}, ${color.g*255}, ${color.b*255})`;
 }
